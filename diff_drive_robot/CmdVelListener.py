5714a37dcf072c1afe87b549b67d036232a4a058
jpgtzg <58833781+jpgtzg@users.noreply.github.com>
2024-05-29 22:45:50 -0600

    Initial Commit via GitAuto

---
+++ diff_drive_robot/CmdVelListener.py
@@ -0,0 +1,30 @@
+# Written by Juan Pablo Gutiérrez
+# 29 05 2025
+
+import rclpy
+from rclpy.node import Node
+
+from geometry_msgs.msg import Twist
+
+class CmdVelListener(Node):
+    def __init__(self):
+        super().__init__('cmd_vel_listener')
+        self.create_subscription(Twist, 'velocity', self.cmd_vel_callback, 10)
+        self.get_logger().info('CmdVelListener has been started.')
+
+    def cmd_vel_callback(self, msg : Twist):
+        self.get_logger().info('Received a message: \n Linear: \n x = {} \n y = {} \n z = {} \n\n Angular: \n x = {} \n y = {} \n z = {}'.format(msg.linear.x , msg.linear.y, msg.linear.z, msg.angular.x, msg.angular.y, msg.angular.z))
+        
+def main(args=None):
+    rclpy.init(args=args)
+
+    cmdvel_listener = CmdVelListener()
+
+    rclpy.spin(cmdvel_listener)
+
+    cmdvel_listener.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
